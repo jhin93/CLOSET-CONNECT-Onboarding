83eb07fbf1faf81e4255d4a33f48b1e3878060d7
jhin93 <scene1993@gmail.com>
2023-07-28 18:03:11 +0900

    데이터 GET 완료

--- CLOSET-CONNECT Store/src/App.js
+++ CLOSET-CONNECT Store/src/App.js
@@ -2,13 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [dataList, setDataList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
+  const fetchData = () => {
+    axios.get('https://closet-recruiting-api.azurewebsites.net/api/data')
+      .then((response) => {
+        // 요청이 성공하면 데이터를 상태에 저장합니다.
+        console.log("response.data : ", response.data)
+        setDataList(response.data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        // 요청이 실패하면 에러를 상태에 저장합니다.
+        setDataList([]);
+        setLoading(false);
+        setError('Error fetching data.');
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
+        <div>
+          <h2>Data List</h2>
+          <ul>
+            {dataList.map((item) => (
+              <li>{item.title}</li>
+            ))}
+          </ul>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
