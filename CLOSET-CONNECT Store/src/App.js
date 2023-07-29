ec052fa1a8183c534a015f69378b5dce2cd8b3eb
jhin93 <scene1993@gmail.com>
2023-07-29 21:55:06 +0900

    화면 노출 완료

--- CLOSET-CONNECT Store/src/App.js
+++ CLOSET-CONNECT Store/src/App.js
@@ -1,94 +1,114 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
-import { useEffect, useState } from 'react';
-import axios from "axios";
+// App.js
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { PricingOption, setPricingOption, setSearchKeyword } from './app/store';
+import axios from 'axios';
 
-function App() {
-  const [dataList, setDataList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  
-  
+const App = () => {
+  const dispatch = useDispatch();
+  const [searchInput, setSearchInput] = useState('');
+
+  const pricingOptions = useSelector((state) => state.pricingOptions);
+  const searchKeyword = useSelector((state) => state.searchKeyword);
+  const [contents, setContents] = useState([]);
+
   useEffect(() => {
-    fetchData();
-  }, []);
-  
-  const fetchData = () => {
+    // 여기에 데이터를 가져오는 API 엔드포인트를 입력하세요.
+    // 예시: axios.get('https://api.example.com/contents')
     axios.get('https://closet-recruiting-api.azurewebsites.net/api/data')
       .then((response) => {
-        // 요청이 성공하면 데이터를 상태에 저장합니다.
-        console.log("response.data : ", response.data)
-        setDataList(response.data);
-        setLoading(false);
-        setError(null);
+        setContents(response.data);
       })
       .catch((error) => {
-        // 요청이 실패하면 에러를 상태에 저장합니다.
-        setDataList([]);
-        setLoading(false);
-        setError('Error fetching data.');
+        console.error('Error fetching data:', error);
       });
+  }, []);
+
+  const filteredContents = contents.filter((content) => {
+    // Pricing Option으로 필터링
+    if (pricingOptions.length === 0) return true;
+    return pricingOptions.includes(content.pricingOption);
+  });
+
+  const searchResult = filteredContents.filter((content) => {
+    // 검색 키워드로 필터링
+    if (searchKeyword === '') return true;
+    return (
+      content.creator.toLowerCase().includes(searchKeyword.toLowerCase()) ||
+      content.title.toLowerCase().includes(searchKeyword.toLowerCase())
+    );
+  });
+
+  const handlePricingOptionChange = (option) => {
+    const updatedOptions = pricingOptions.includes(option)
+      ? pricingOptions.filter((item) => item !== option)
+      : [...pricingOptions, option];
+
+    dispatch(setPricingOption(updatedOptions));
+  };
+
+  const handleSearchInputChange = (event) => {
+    const keyword = event.target.value;
+    setSearchInput(keyword);
+    dispatch(setSearchKeyword(keyword));
+  };
+
+  const handleReset = () => {
+    dispatch(setPricingOption([]));
+    dispatch(setSearchKeyword(''));
+    setSearchInput('');
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <div>
-          <h2>Data List</h2>
-          <ul>
-            {dataList.map((item) => (
-              <li>{item.title}</li>
-            ))}
-          </ul>
-        </div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      <h1>Contents List</h1>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={pricingOptions.includes(PricingOption.PAID)}
+            onChange={() => handlePricingOptionChange(PricingOption.PAID)}
+          />
+          Paid Option
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={pricingOptions.includes(PricingOption.FREE)}
+            onChange={() => handlePricingOptionChange(PricingOption.FREE)}
+          />
+          Free Option
+        </label>
+        <label>
+          <input
+            type="checkbox"
+            checked={pricingOptions.includes(PricingOption.VIEW_ONLY)}
+            onChange={() => handlePricingOptionChange(PricingOption.VIEW_ONLY)}
+          />
+          View Only Option
+        </label>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={searchInput}
+          onChange={handleSearchInputChange}
+          placeholder="Search by name or title"
+        />
+      </div>
+      <ul>
+        {searchResult.map((content) => (
+          <li key={content.id}>
+            <img src={content.imagePath} alt={content.title} />
+            <p>{content.title}</p>
+            <p>Creator: {content.creator}</p>
+            <p>Pricing Option: {content.pricingOption}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- CLOSET-CONNECT Store/src/app/store.js
+++ CLOSET-CONNECT Store/src/app/store.js
@@ -1,8 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// store.js
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+// Enum for PricingOption
+export const PricingOption = {
+  PAID: 0,
+  FREE: 1,
+  VIEW_ONLY: 2,
+};
+
+const initialState = {
+  pricingOptions: [],
+  searchKeyword: '',
+};
+
+const contentSlice = createSlice({
+  name: 'content',
+  initialState,
+  reducers: {
+    setPricingOption(state, action) {
+      state.pricingOptions = action.payload;
+    },
+    setSearchKeyword(state, action) {
+      state.searchKeyword = action.payload;
+    },
   },
+});
+
+export const { setPricingOption, setSearchKeyword } = contentSlice.actions;
+
+// Store
+export default configureStore({
+  reducer: contentSlice.reducer,
 });
